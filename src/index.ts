ae7a413809c0b58729a369f96ce861d5e61fcce5
Kankgan Hanmungtham <kanokgan@gmail.com>
2020-09-21 17:14:57 +0700

    Done typescript environment config.
    Start working on db.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log("Hello There")
+import { MikroORM } from "@mikro-orm/core";
+import { Post } from "entities/Post";
+import __prod__ from 'constants';
+
+
+const main = async () => {
+    const orm = await MikroORM.init({
+        entities: [Post],
+        dbName: 'lireddit',
+        type: 'postgresql',
+        debug: !__prod__,
+    });
+
+    console.log(orm != null)
+}
+
+main();
